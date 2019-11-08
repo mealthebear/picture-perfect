277b79e9687a46b883cdda32b34423bb447ead95
Dennis <mealthebear@gmail.com>
2019-11-07 18:33:23 -0800

    Created client list template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ClientList from './ClientList.js';
 
 class App extends React.Component {
     constructor(props) {
@@ -8,7 +9,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>This is App</div>
+            <div>
+                <ClientList />
+            </div>
         )
     }
 }
---
+++ client/src/components/ClientList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClientList = (props) => (
+    <div>
+        <p>First Name</p>
+        <p>Last Name</p>
+        <p>Cost of Service</p>
+        <p>Description of Service</p>
+    </div>
+)
+
+export default ClientList;

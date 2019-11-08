ed7d381a695421c527aa8f6ca7bcc4c6312991c4
Dennis <mealthebear@gmail.com>
2019-11-07 16:47:20 -0800

    Included router in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const router = require('./router.js');
 
 const app = express();
 const port = 3000;
@@ -8,5 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Listening on port ${port} woo!`));

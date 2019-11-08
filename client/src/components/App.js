b2a0151785697b650f519161736dd415df7d9f3f
Dennis <mealthebear@gmail.com>
2019-11-07 19:01:36 -0800

    Added title of website to the page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <h1>Picture Perfect</h1>
                 <ClientList />
                 <ClientForm />
             </div>

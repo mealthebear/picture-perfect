a723865866d7b47e65727543893d6cc2d1b3f488
Dennis <mealthebear@gmail.com>
2019-11-08 19:01:30 -0800

    Created handler functions in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ClientList from './ClientList.js';
 import ClientForm from './ClientForm.js';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {};
+        this.state = {
+
+        };
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    handleChange(e) {
+
+    }
+
+    handleSubmit(e) {
+
     }
 
     render() {

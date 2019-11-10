57f3b7eb00ec2703cee6ebea5a72fb2b39edd2b1
Dennis <mealthebear@gmail.com>
2019-11-09 18:52:35 -0800

    Added headings to make text more readable

--- client/src/components/ClientList.js
+++ client/src/components/ClientList.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ClientList = (props) => (
     <div>
         {props.clients.map((client, i) => {
-            return <div key={i}>{client.id}{client.firstName}{client.lastName}
-                <div>{client.bill}</div>
-                <div>{client.description}</div>
+            return <div key={i}><h2>{client.id}{client.firstName}{client.lastName}</h2>
+                <div><h3>{client.bill}</h3></div>
+                <div><h4>{client.description}</h4></div>
             </div>
         })}
     </div>

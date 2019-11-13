638fb91473b99b20d6a39215e034a73608b375bd
Dennis <mealthebear@gmail.com>
2019-11-12 20:30:05 -0800

    Made input fields stack vertically in form

--- client/src/components/ClientForm.js
+++ client/src/components/ClientForm.js
@@ -3,14 +3,22 @@ import React from 'react';
 const ClientForm = (props) => (
     <div>
         <form onSubmit={props.onSubmit}>
+            <div>
             <label>First Name</label>
             <input type="text" name="firstName" onChange={props.onChange}></input>
+            </div>
+            <div>
             <label>Last Name</label>
             <input type="text" name="lastName" onChange={props.onChange}></input>
+            </div>
+            <div>
             <label>Bill</label>
             <input type="text" name="bill" onChange={props.onChange}></input>
+            </div>
+            <div>
             <label>Description</label>
             <input type="text" name="description" onChange={props.onChange}></input>
+            </div>
             <input type="submit" value="Submit"></input>
         </form>
     </div>

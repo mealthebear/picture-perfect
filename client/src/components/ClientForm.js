4acc8ae8e4c5086d4dba6ac8fb75e979a1c8718d
Dennis <mealthebear@gmail.com>
2019-11-15 18:44:35 -0800

    Implemented CSS Grid on client form

--- client/src/components/ClientForm.js
+++ client/src/components/ClientForm.js
@@ -3,18 +3,14 @@ import React from 'react';
 const ClientForm = (props) => (
     <div className="client-form-box">
         <form className="client-form" onSubmit={props.onSubmit}>
-            <div className="client-labels">
-                <label>First Name</label>
-                <label>Last Name</label>
-                <label>Bill</label>
-                <label>Description</label>
-            </div>
-            <div className="client-inputs">
+                <label className="label-1">First Name</label>
                 <input type="text" name="firstName" onChange={props.onChange}></input>
+                <label>Last Name</label>
                 <input type="text" name="lastName" onChange={props.onChange}></input>
+                <label>Bill</label>
                 <input type="text" name="bill" onChange={props.onChange}></input>
+                <label>Description</label>
                 <input type="text" name="description" onChange={props.onChange}></input>
-            </div>
             <input type="submit" value="Submit"></input>
         </form>
     </div>

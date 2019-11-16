787793080936fb188a19958dc37c14d833d44c01
Dennis <mealthebear@gmail.com>
2019-11-15 17:44:44 -0800

    Reformatted and cleaned up client form code

--- client/src/components/ClientForm.js
+++ client/src/components/ClientForm.js
@@ -3,21 +3,17 @@ import React from 'react';
 const ClientForm = (props) => (
     <div>
         <form className="client-form" onSubmit={props.onSubmit}>
-            <div>
-            <label>First Name</label>
-            <input type="text" name="firstName" onChange={props.onChange}></input>
+            <div className="client-labels">
+                <label>First Name</label>
+                <label>Last Name</label>
+                <label>Bill</label>
+                <label>Description</label>
             </div>
-            <div>
-            <label>Last Name</label>
-            <input type="text" name="lastName" onChange={props.onChange}></input>
-            </div>
-            <div>
-            <label>Bill</label>
-            <input type="text" name="bill" onChange={props.onChange}></input>
-            </div>
-            <div>
-            <label>Description</label>
-            <input type="text" name="description" onChange={props.onChange}></input>
+            <div className="client-inputs">
+                <input type="text" name="firstName" onChange={props.onChange}></input>
+                <input type="text" name="lastName" onChange={props.onChange}></input>
+                <input type="text" name="bill" onChange={props.onChange}></input>
+                <input type="text" name="description" onChange={props.onChange}></input>
             </div>
             <input type="submit" value="Submit"></input>
         </form>

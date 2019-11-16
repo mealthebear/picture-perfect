dead44f27799e4df1f1ad067752cc4e40a7dfdd1
Dennis <mealthebear@gmail.com>
2019-11-15 19:00:57 -0800

    Developed styling for inputs and labels

--- client/src/components/ClientForm.js
+++ client/src/components/ClientForm.js
@@ -3,14 +3,14 @@ import React from 'react';
 const ClientForm = (props) => (
     <div className="client-form-box">
         <form className="client-form" onSubmit={props.onSubmit}>
-                <label className="label-1">First Name</label>
-                <input type="text" name="firstName" onChange={props.onChange}></input>
-                <label>Last Name</label>
-                <input type="text" name="lastName" onChange={props.onChange}></input>
-                <label>Bill</label>
-                <input type="text" name="bill" onChange={props.onChange}></input>
-                <label>Description</label>
-                <input type="text" name="description" onChange={props.onChange}></input>
+                <label className="client-label">First Name</label>
+                <input className="client-input" type="text" name="firstName" onChange={props.onChange}></input>
+                <label className="client-label">Last Name</label>
+                <input className="client-input" type="text" name="lastName" onChange={props.onChange}></input>
+                <label className="client-label">Bill</label>
+                <input className="client-input" type="text" name="bill" onChange={props.onChange}></input>
+                <label className="client-label">Description</label>
+                <input className="client-input" type="text" name="description" onChange={props.onChange}></input>
             <input className="client-submit" type="submit" value="Submit"></input>
         </form>
     </div>

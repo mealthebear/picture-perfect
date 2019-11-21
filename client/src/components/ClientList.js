b7173c6b155f8b6cad7eb84da3e2e6a19e9a21aa
Dennis <mealthebear@gmail.com>
2019-11-20 19:52:21 -0800

    Developed styling to center client list entries

--- client/src/components/ClientList.js
+++ client/src/components/ClientList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ClientList = (props) => (
-    <div>
+    <div className="client-list-container">
         {props.clients.map((client, i) => {
             return <div className="client-list-box" key={i}><h2 className="client-name">{client.firstName} {client.lastName}</h2>
                 <div><h3 className="client-bill">{props.numberToString(client.bill)}</h3></div>

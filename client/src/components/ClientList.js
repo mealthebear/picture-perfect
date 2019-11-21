c6416a9d60f34482d869c7cebeab2744a1a91f79
Dennis <mealthebear@gmail.com>
2019-11-20 19:39:50 -0800

    Implemented comma adder into client list

--- client/src/components/ClientList.js
+++ client/src/components/ClientList.js
@@ -4,7 +4,7 @@ const ClientList = (props) => (
     <div>
         {props.clients.map((client, i) => {
             return <div className="client-list-box" key={i}><h2 className="client-name">{client.firstName} {client.lastName}</h2>
-                <div><h3 className="client-bill">${client.bill}</h3></div>
+                <div><h3 className="client-bill">${props.numberToString(client.bill)}</h3></div>
                 <div className="client-description-box"><h4 className="client-description">{client.description}</h4></div>
                 <div data-identifier={client.id} className="client-delete" onClick={props.deleteClient}>Delete Entry</div>
             </div>

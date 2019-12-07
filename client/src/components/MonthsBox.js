7f75497d6e9574b14fee08955fa07648964bc5e7
Dennis <mealthebear@gmail.com>
2019-12-07 00:27:35 -0800

    Applied change month function to every month

--- client/src/components/MonthsBox.js
+++ client/src/components/MonthsBox.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const MonthsBox = (props) => (
     <div className="date-container">
-        <p name="month">January</p>
-        <p name="month">February</p>
-        <p name="month">March</p>
-        <p name="month">April</p>
-        <p name="May">May</p>
-        <p name="June">June</p>
-        <p name="July">July</p>
-        <p name="August">August</p>
-        <p name="September">September</p>
-        <p name="October">October</p>
-        <p name="November">November</p>
-        <p name="December">December</p>
+        <p name="month" onClick={props.changeMonth}>January</p>
+        <p name="month" onClick={props.changeMonth}>February</p>
+        <p name="month" onClick={props.changeMonth}>March</p>
+        <p name="month" onClick={props.changeMonth}>April</p>
+        <p name="month" onClick={props.changeMonth}>May</p>
+        <p name="month" onClick={props.changeMonth}>June</p>
+        <p name="month" onClick={props.changeMonth}>July</p>
+        <p name="month" onClick={props.changeMonth}>August</p>
+        <p name="month" onClick={props.changeMonth}>September</p>
+        <p name="month" onClick={props.changeMonth}>October</p>
+        <p name="month" onClick={props.changeMonth}>November</p>
+        <p name="month" onClick={props.changeMonth}>December</p>
     </div>
 )
 

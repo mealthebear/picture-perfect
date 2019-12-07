e6b4e90e2b981e78fbfda5ad625ed1254421e2b5
Dennis <mealthebear@gmail.com>
2019-12-06 22:11:42 -0800

    Built out component with months of the year

---
+++ client/src/components/MonthsBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MonthsBox = (props) => (
+    <div>
+        <p name="January">January</p>
+        <p name="February">February</p>
+        <p name="March">March</p>
+        <p name="April">April</p>
+        <p name="May">May</p>
+        <p name="June">June</p>
+        <p name="July">July</p>
+        <p name="August">August</p>
+        <p name="September">September</p>
+        <p name="October">October</p>
+        <p name="November">November</p>
+        <p name="December">December</p>
+    </div>
+)
+
+export default MonthsBox;
